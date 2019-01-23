c4ae945b1ea3e83467a051ff80aa27d7119b7d2a
Inconnu08 <taufiqrx8@gmail.com>
2019-01-23 14:42:05 +0600

    fix 832

--- flipping_an_image.py
+++ flipping_an_image.py
@@ -3,10 +3,12 @@ def flipAndInvertImage(A):
     :type A: List[List[int]]
     :rtype: List[List[int]]
     """
-    for i, l in enumerate(A):
-        print(l[i])
+    ls = []
+    for l in A:
+        l.reverse()
+        ls.append([1 if x == 0 else 0 for x in l])
 
-    return A
+    return ls
 
 
 print(flipAndInvertImage([[1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0]]))
