f072af6acb295da1f05f6f793b510cc9b6e4d216
Inconnu08 <taufiqrx8@gmail.com>
2019-01-29 23:46:37 +0600

    bug hammingdistance

--- hamming_distance.py
+++ hamming_distance.py
@@ -1,3 +1,6 @@
+import operator
+
+
 def hammingDistance(x, y):
     x = format(x, '04b')
     y = format(y, '04b')
@@ -12,6 +15,19 @@ def idx(i):
         return 0
 
 
+def hamming2(str1, str2):
+    str1 = format(str1, '04b')
+    str2 = format(str2, '04b')
+    assert len(str1) == len(str2)
+
+    ne = operator.ne
+    return sum(map(ne, str1, str2))
+
+
 print(hammingDistance(1, 4))
 print(hammingDistance(0, 1))
 print(hammingDistance(93, 73))
+
+print(hamming2(1, 4))
+print(hamming2(0, 1))
+print(hamming2(93, 73))
